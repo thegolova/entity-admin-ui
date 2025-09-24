878cecdcc2866b307f12a0246a43173c25acaa2e
Artem Golovinskiy <thegolova@gmail.com>
2025-09-24 08:25:48 +0300

    small fixes, initial Table component

---
+++ src/features/table/Table.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+
+const Table = ({ data, columns }) => {
+  return (
+    <div>
+      <table>
+        <thead>
+          {columns.map((column) => {
+            <th key={column.key}>{column.label}</th>;
+          })}
+        </thead>
+        <tbody>
+          {data.lenght === 0 ? (
+            <tr>
+              <td> no data </td>
+            </tr>
+          ) : (
+            data.map((item) => (
+              <tr key={item.id}>
+                {
+                  columns.map((column) => (
+                    <td key={column.id}>
+                      {column.extra ? column.extra(item) : item[column.key]}
+                    </td>
+                  ))
+                }
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
