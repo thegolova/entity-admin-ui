3851815edc51733b597028e7755a20c2f498d1e7
Artem Golovinskiy <thegolova@gmail.com>
2025-09-29 18:17:41 +0300

    fix column types

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,35 +5,38 @@ import Table from "@/features/table/Table";
 import { ColumnType } from "@/features/table/types";
 import { useStore } from "@/store/useStore";
 import { ProductsType } from "@/types/entities";
-import React from "react";
+import React, { useMemo } from "react";
 
 const ProductsPage = () => {
   const { products, updateProduct } = useStore();
 
-  const columns: ColumnType<ProductsType>[] = [
-    { key: "id", label: "ID", type: "number" },
-    { key: "name", label: "Name", type: "string" },
-    {
-      key: "options.size",
-      label: "Size",
-      type: "enum",
-      enumValues: ["S", "M", "L", "XL"],
-    },
-    { key: "options.amount", label: "Amount", type: "number" },
-    {
-      key: "active",
-      label: "Active",
-      render: (item: ProductsType) => (item.active ? "yes" : "no"),
-      type: "boolean",
-    },
-    {
-      key: "createdAt",
-      label: "Created At",
-      render: (item: ProductsType) =>
-        new Date(item.createdAt).toLocaleDateString(),
-      type: "date",
-    },
-  ];
+  const columns = useMemo<ColumnType<ProductsType>[]>(
+    () => [
+      { key: "id", label: "ID", type: "number" },
+      { key: "name", label: "Name", type: "string" },
+      {
+        key: "options.size",
+        label: "Size",
+        type: "enum",
+        enumValues: ["S", "M", "L", "XL"],
+      },
+      { key: "options.amount", label: "Amount", type: "number" },
+      {
+        key: "active",
+        label: "Active",
+        render: (item: ProductsType) => (item.active ? "yes" : "no"),
+        type: "boolean",
+      },
+      {
+        key: "createdAt",
+        label: "Created At",
+        render: (item: ProductsType) =>
+          new Date(item.createdAt).toLocaleDateString(),
+        type: "date",
+      },
+    ],
+    []
+  );
 
   return (
     <div className="p-6">
